0a6eaf8f67ef7be77c0d7eaac4cbd4b87729b564
YoungThomas <623491964@qq.com>
2015-06-24 16:57:39 +0800

    杨鹏

---
+++ Application/Home/Controller/IndexController.class.php
@@ -0,0 +1,9 @@
+<?php
+namespace Home\Controller;
+use Think\Controller;
+class IndexController extends Controller {
+	public function index() {
+		redirect(U('Comments/comments'), 0, "redirect to comments");
+	}
+
+}
